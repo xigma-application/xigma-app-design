16cf7cc8140671cf12f33fd58746d52cc75ed85a
Eryk Sandecki <eryk.sandecki@gmail.com>
2026-08-18 15:43:26 +0200

    XG-APP: fix icons sizes

--- src/components/Design/Toolbar/constants.ts
+++ src/components/Design/Toolbar/constants.ts
@@ -63,12 +63,9 @@ export const TOOL_GROUP_ITEMS: Partial<Record<ToolName, ToolName[]>> = {
 };
 
 export const TOOL_ICON_SIZE: Partial<Record<ToolName, number>> = {
-  [ToolName.arrow]: 24,
-  [ToolName.line]: 24,
   [ToolName.polygon]: 17,
   [ToolName.slice]: 18,
-  [ToolName.star]: 24,
-  [ToolName.textOnPath]: 18,
+  [ToolName.star]: 20,
 };
 
 export const TOOLS_WITH_DROPDOWN: ToolName[] = [ToolName.default, ToolName.frame, ToolName.rectangle, ToolName.text];
